f17e6d47650058e6b180e5ce68168bf77a2dd975
Mahbubur Rahman <durlovrahman32@gmail.com>
2024-07-11 02:16:44 +0600

    Create About.js

---
+++ src/routes/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Abthead from '../components/Abthead';
+import Edu from '../components/Edu';
+import Office from '../components/Office';
+import PrbSlv from '../components/PrbSlv';
+import Certifications from '../components/Certifications';
+
+function About() {
+  return (
+    <>
+    <Abthead />
+     <Edu />
+    <Office />
+    <PrbSlv />
+    <Certifications />
+    </>
+  );
+}
+
+export default About
